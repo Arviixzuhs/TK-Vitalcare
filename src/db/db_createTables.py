d1d53340e9b94d8b5f8bab863fb6d0a62435c3de
Arviixzuh <arviixzuh@gmail.com>
2024-06-22 00:36:34 -0400

    feat: 📦 implementación de las tablas para manejar consultas, pacientes y personal médico

--- src/db/db_createTables.py
+++ src/db/db_createTables.py
@@ -3,9 +3,6 @@ import sqlite3
 
 def dbCreateTables():
     conexion = sqlite3.connect("database.db")
-    conexion.execute(
-        """CREATE TABLE IF NOT EXISTS product (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL)"""
-    )
 
     conexion.execute(
         """
@@ -16,4 +13,42 @@ def dbCreateTables():
             password TEXT NOT NULL
         )
     """
+    )
+
+    conexion.execute(
+        """
+        CREATE TABLE IF NOT EXISTS consult (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            reason TEXT NOT NULL,
+            doctor_id INTEGER NOT NULL,
+            patient_id INTEGER NOT NULL
+        )
+    """
+    )
+
+    conexion.execute(
+        """CREATE TABLE IF NOT EXISTS patient (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name TEXT, 
+            last_name TEXT,
+            dni INT,
+            phone INT,
+            address TEXT,
+            diagnosis TEXT
+            )
+        """
+    )
+
+    conexion.execute(
+        """CREATE TABLE IF NOT EXISTS clinicalstaff (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name TEXT, 
+            last_name TEXT,
+            dni INT,
+            phone INT,
+            address TEXT,
+            position TEXT
+            )
+        """
     )
