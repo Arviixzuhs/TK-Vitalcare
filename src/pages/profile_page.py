c15421ba854ef2192893d7681de7a106130c5bff
Arviixzuh <arviixzuh@gmail.com>
2024-06-22 00:35:47 -0400

    style: 🌈 corrección de detalles

--- src/pages/profile_page.py
+++ src/pages/profile_page.py
@@ -12,24 +12,24 @@ class ProfilePage(Page):
         self.user_id = user[0]
 
         # User profile tab
-        self.frame = LabelFrame(self, text="User Profile")
+        self.frame = LabelFrame(self, text="Perfil de usuario")
         self.frame.grid(row=0, column=0, columnspan=2, pady=20, padx=20)
 
         # Input Name
-        Label(self.frame, text="Name: ").grid(row=1, column=0, sticky="w")
+        Label(self.frame, text="Nombre: ").grid(row=1, column=0, sticky="w")
         self.name_entry = Entry(self.frame)
         self.name_entry.grid(row=1, column=1, sticky="w")
         self.name_entry.insert(0, user[1])
 
         # Input Last Name
-        Label(self.frame, text="Last Name: ").grid(row=2, column=0, sticky="w")
+        Label(self.frame, text="Apellido: ").grid(row=2, column=0, sticky="w")
         self.last_name_entry = Entry(self.frame)
         self.last_name_entry.grid(row=2, column=1, sticky="w")
         self.last_name_entry.insert(0, user[2])
 
         # Update Profile Button
         self.update_button = Button(
-            self.frame, text="Update Profile", command=self.update_profile
+            self.frame, text="Actualizar perfil", command=self.update_profile
         )
         self.update_button.grid(row=5, columnspan=2, sticky=W + E)
 
@@ -38,11 +38,11 @@ class ProfilePage(Page):
         self.message_1.grid(row=4, columnspan=2, sticky=W + E)
 
         # User change password tab
-        self.change_password_frame = LabelFrame(self, text="Change password")
+        self.change_password_frame = LabelFrame(self, text="Cambiar contraseña")
         self.change_password_frame.grid(row=3, column=0, columnspan=2, pady=20, padx=20)
 
         # Input Password
-        Label(self.change_password_frame, text="Password: ").grid(
+        Label(self.change_password_frame, text="Contraseña: ").grid(
             row=3, column=0, sticky="w"
         )
         self.password_entry = Entry(self.change_password_frame, show="*")
@@ -51,7 +51,7 @@ class ProfilePage(Page):
         # Change password button
         self.update_button = Button(
             self.change_password_frame,
-            text="Change password",
+            text="Cambiar contraseña",
             command=self.change_password,
         )
         self.update_button.grid(row=5, columnspan=2, sticky=W + E)
@@ -66,7 +66,7 @@ class ProfilePage(Page):
         last_name = self.last_name_entry.get()
 
         if not name or not last_name:
-            return self.message_1.config(text="Name, Last Name cannot be empty")
+            return self.message_1.config(text="Todos los campos son requeridos")
 
         response_data = UserService.update_profile(
             parameters=(name, last_name, self.user_id)
@@ -77,7 +77,7 @@ class ProfilePage(Page):
     def change_password(self):
         password = self.password_entry.get()
         if not password:
-            return self.message_2.config(text="Password cannot be empty")
+            return self.message_2.config(text="Todos los campos son requeridos")
 
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
         response_data = UserService.update_password(
